7e46f80b37809c8a0d64b65ee341b3c274138757
panhaidong <panhaidon@qq.com>
2019-01-15 18:26:55 +0800

    初始化

---
+++ config-client/src/main/java/pers/ph/configclient/contorller/FooController.java
@@ -0,0 +1,30 @@
+package pers.ph.configclient.contorller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Pundix055
+ * @date 2019-01-15 15:38.
+ */
+@Configuration
+@EnableAutoConfiguration
+@RestController
+@Slf4j
+public class FooController {
+    @Value("${foo:ttttt}")
+    private String foo;
+
+    @Value("${overview}")
+    private String overview;
+
+    @GetMapping(path = "/test")
+    public String getFoo() {
+        log.info("foo:{}, overview:{}", foo, overview);
+        return foo + "-" + overview;
+    }
+}
