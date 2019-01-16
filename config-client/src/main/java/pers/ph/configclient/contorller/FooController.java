641b5985d636b28d2e42250572aefa000ef91e03
panhaidong <panhaidon@qq.com>
2019-01-16 16:54:26 +0800

    使用Spring Cloud bus 动态获取配置更新

--- config-client/src/main/java/pers/ph/configclient/contorller/FooController.java
+++ config-client/src/main/java/pers/ph/configclient/contorller/FooController.java
@@ -3,6 +3,7 @@ package pers.ph.configclient.contorller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Configuration
 @EnableAutoConfiguration
 @RestController
+@RefreshScope
 @Slf4j
 public class FooController {
     @Value("${foo:default-foo}")
