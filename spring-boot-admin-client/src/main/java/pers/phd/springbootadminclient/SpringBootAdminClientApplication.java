a54313a50ae3129436f252ae622f7c0dad8c65b2
panhaidong <panhaidon@qq.com>
2019-01-18 18:37:53 +0800

    增加SpringBootAdmin监控程序和提供给监控的程序

---
+++ spring-boot-admin-client/src/main/java/pers/phd/springbootadminclient/SpringBootAdminClientApplication.java
@@ -0,0 +1,16 @@
+package pers.phd.springbootadminclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringBootAdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminClientApplication.class, args);
+    }
+
+}
+
---
+++ spring-boot-admin/src/main/java/pers/phd/springbootadmin/SpringBootAdminApplication.java
@@ -0,0 +1,18 @@
+package pers.phd.springbootadmin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@EnableAdminServer
+@SpringBootApplication
+public class SpringBootAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminApplication.class, args);
+    }
+
+}
+
