e98c33741a8e26092045632208d7c204cbe976c9
panhaidong <240760715@qq.com>
2019-05-09 23:35:25 +0800

    更新Spring cloud 版本为 Greenwich.SR1 更新Spring boot 版本为 2.1.4 更新获取配置的配置文件，允许本地覆盖远程配置

--- config-client/src/main/java/pers/ph/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/pers/ph/configclient/ConfigClientApplication.java
@@ -3,8 +3,10 @@ package pers.ph.configclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class ConfigClientApplication {
 
